732bfb48b2f08020ae9bdfcb5c5c9dd18a0857d9
Ali Usman <ali.usmansajid02@gmail.com>
2024-04-27 00:37:15 +0500

    fonts setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,33 @@
 import { Link } from 'react-router-dom';
+import menuIcon from '../assets/images/menu.svg';
+import cross from '../assets/images/cross.svg';
 
 
 const Header = () => {
   return (
-    <>
-      <div className='header'>Header</div>
-      <div className="nav">
-        <Link to='/contact'>Contact</Link>
-        <Link to='/'>Home</Link>
+    <header>
+      <div className="header-wrap">
+        <div className="links">
+          <a href="#" className="link">Home</a>
+          <a href="#" className="link">Residences</a>
+          <a href="#" className="link">Amenities</a>
+          <a href="#" className="link">Availabilities</a>
+          <a href="#" className="link">Neighborhood</a>
+          <a href="#" className="link">Contact</a>
+        </div>
+        <div className="logo-wrap">
+          <img className="logo" src="./assets/images/1946_Ocean_Logo_White.png" alt=""/>
+        </div>
+        <div className="schedule-button">
+          <a href="#">Schedule A Tour</a>
+        </div>
+        <div className="menu-wrap">
+          <img src={menuIcon} alt="" />
+          <img src={cross} alt="" />
+          <span>Menu</span>
+        </div>
       </div>
-      <hr />
-    </>
+  </header>
   )
 }
 
