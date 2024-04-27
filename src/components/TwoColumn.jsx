ac1017d7fadb86f76ddc3193f4b17fc35d824ccb
Qaim.Prystech <qaimprystech@gmail.com>
2024-04-27 06:01:05 +0500

    Twocolumn-Styling

--- src/components/TwoColumn.jsx
+++ src/components/TwoColumn.jsx
@@ -1,25 +1,40 @@
-import UnitDetailsImg from '../assets/images/home/unit-details-image.png'; 
-
 const TwoColumn = ({data}) => {
     const {backgroundColor, heading, subheading, description, image, flexDirection, logo} = data;
     return (  
         <>
-            <section className="unit-details">
-                <div className="inner">
+            <section className={heading.text} style= {{
+                    background:backgroundColor,
+                    padding:"0px 40px"
+                }} >
+                <div className="inner" style= {{
+                        display:"flex",
+                        flexDirection:flexDirection,
+                        justifyContent:"space-between",
+                        span:{
+                            color:subheading.secondaryColor,
+                        }
+                    }} >
+
                     <div className="image-wrap">
-                        <img src={UnitDetailsImg} alt="" />
+
+                        <img src={image} alt="image" />
+
                     </div>
-                    <div className="content-wrap">
-                        <div className="heading">
 
-                        </div>
-                        <div className="sub-heading">
+                    <div className="content-wrap" style= {{
+                            display:"flex",
+                            flexDirection:"column",
+                        }} >
 
+                        <div className="heading" style={{color:heading.color}}> {heading.text} </div>
+                        <div className="sub-heading" style={{color:subheading.color}} dangerouslySetInnerHTML={{ __html: subheading.text }} />
+                        <div className="description" style={{color:description.color}} dangerouslySetInnerHTML={{ __html: description.text }} />
+                        <div className="logo" >
+                            <img src={logo} alt="logo" />
                         </div>
-                        <div className="description">
 
-                        </div>
                     </div>
+
                 </div>
             </section>
         </>
