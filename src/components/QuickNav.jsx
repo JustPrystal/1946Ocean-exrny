02d950a7dd9945f913477e4d0e7b59a6729a2710
Anas <anashussain2711@gmail.com>
2024-05-03 06:04:32 +0500

    fixes

--- src/components/QuickNav.jsx
+++ src/components/QuickNav.jsx
@@ -1,19 +1,21 @@
+import { Link } from "react-router-dom";
+
 const QuickNav = ({links}) => {
     const {prev, next} = links
     return ( 
         <section class="quick-navigation">
             <div class="inner">
-                <a class="prev-page" href={prev.url}>
+                <Link class="prev-page" to={prev.url}>
                     <svg className="arrow leftarrow" width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#FAE9D7" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M13.26 15.53L9.73999 12L13.26 8.46997" stroke="#FAE9D7" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                     <p>{prev.title}</p>
-                </a>
+                </Link>
                 <div class="heading-wrap">
                     <h2> Schedule a Tour </h2>
                 </div>
-                <a class="next-page" href={next.url}>
+                <Link class="next-page" to={next.url}>
                     <p>{next.title}</p>
                     <svg className='arrow rightarrow' width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#FAE9D7" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M10.74 15.53L14.26 12L10.74 8.46997" stroke="#FAE9D7" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-                </a>
+                </Link>
             </div>
         </section>
     );
