8e6d65fc431bd7fdfed79f430257841664ede86b
Sameer <sameer.prystech@gmail.com>
2024-05-04 02:07:04 +0500

    scrollup button fixed

--- src/components/ScrollUpButton.jsx
+++ src/components/ScrollUpButton.jsx
@@ -1,8 +1,11 @@
 import UpArrow from '../assets/images/up-arrow.svg'
 
 const ScrollUpButton = () => {
+    const scrollUp = ()=>{
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
     return ( 
-        <a className="up-arrow-circle" href="#header">
+        <a className="up-arrow-circle" onClick={scrollUp}>
             <img className="scroll-up-button" src={UpArrow} alt="" />
         </a>
     );
