a1596e86288e7869c84ce418770431f205c5f7b5
JustPrystal <ali.usmansajid02@gmail.com>
2024-05-04 06:34:58 +0500

    scroll button show set

--- src/components/ScrollUpButton.jsx
+++ src/components/ScrollUpButton.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react';
 import UpArrow from '../assets/images/up-arrow.svg'
 
 const ScrollUpButton = () => {
+
+    const [buttonClasses, setButtonClasses] = useState([])
+
     const scrollUp = ()=>{
         window.scrollTo({ top: 0, behavior: 'smooth' });
     }
+    useEffect(() => {
+        const handleScrollButton = () => {
+          const button = document.querySelector('.up-arrow-circle');
+          if (window.scrollY >= 600 && !button.classList.contains('scrolled')) {
+            setButtonClasses([...buttonClasses, 'scrolled']);
+          } else if (window.scrollY < 100 && button.classList.contains('scrolled')) {
+            setButtonClasses(buttonClasses.filter(className => className !== 'scrolled'));
+          }
+        };
+    
+        document.addEventListener('scroll', handleScrollButton);
+
+      }, [buttonClasses]);
     return ( 
-        <div className="up-arrow-circle" onClick={scrollUp}>
+        <div className={buttonClasses + " up-arrow-circle"} onClick={scrollUp}>
             <img className="scroll-up-button" src={UpArrow} alt="" />
         </div>
     );
